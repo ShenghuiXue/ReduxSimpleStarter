583472689356346fc90b30197c4767d4b0a5b339
Shenghui Xue <sxue41@gatech.edu>
2018-05-23 09:02:34 -0700

    search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-  return <input />;
+class SearchBar extends Component{ // make a class of SearchBar and extend from component
+  // define a state
+  constructor (pross) { // this is a constructor
+    super (pross); // get all the methods from pararent class
+    this.state = {term: "1"}; // set initial state
+  }
+  render() { // render
+    return ( // return search_bar
+      <div>
+        <input
+          value ={this.state.term}
+          onChange={event => this.setState({term: event.target.value})}
+          />
+
+      </div>
+    );
+  }
+
+
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 // javascript modules in ES6.
-import React from 'react';
+import React, {Component} from 'react';
+// import ReactDoM
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
+// import SearchBar from search_bar.js inside of components folder
 import SearchBar from './components/search_bar';
-
+// import key
 const API_KEY = "AIzaSyCk2w7xaSOKqprqBP67CEwW9YoCgE1pQ1k";
 
+
+
 // Create a new component. This component should produce some HTML.
-const App = () => { // const declear a var which the the final varible
-  return (<div>
-  <SearchBar />
-  </div>); // this is a JSX
+class App extends Component {
+  constructor(props) {
+    super (props);
+    this.state = {videos: []};
+    YTSearch ({key:API_KEY, term:'qing yun zhi'}, (videos) =>{
+      this.setState({videos});
+    });
+  }
+  render (){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  };
 }
+
 
 // Take this component html gerenrated html and put it on the page.
 ReactDOM.render(<App />, document.querySelector(".container"));
+
+// how jsx work? defince some var such as const App(). ReactDOM.render(<App />, document.querySelector(some id))
