363d6e844b3f07ffca350ecffda6eb8e189d7ded
Shenghui Xue <sxue41@gatech.edu>
2018-05-31 20:56:07 -0700

    youtube search completed

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,22 +2,34 @@ import React, {Component} from 'react';
 
 class SearchBar extends Component{ // make a class of SearchBar and extend from component
   // define a state
-  constructor (pross) { // this is a constructor
-    super (pross); // get all the methods from pararent class
-    this.state = {term: "1"}; // set initial state
+  // props passed from index.js : onSearchTermChange={videoSearch}
+  constructor (props) { // this is a constructor
+    super (props); // get all the methods from pararent class
+    this.state = {term: ""}; // set initial state each class can have its own state
   }
   render() { // render
-    return ( // return search_bar
-      <div>
-        <input
-          value ={this.state.term}
-          onChange={event => this.setState({term: event.target.value})}
-          />
+    return ( // return search_bar // can only return 1 div?
+      <div className="search-bar">
 
+        <input
+          placeholder= "Search youtube here"
+          value ={this.state.term}
+          onChange={event => this.onInputChange(event.target.value)}
+          />
+          <span> <i class="fas fa-search"></i></span>
       </div>
     );
   }
+  // onInputChange perform two functions:
 
+  onInputChange(term) {
+    // 1. change the state of term to be the valule input by user
+    // this is done in the same file
+    this.setState ({term});
+    // 2. return search term to the pararent file
+    // props is onSearchTermChange={videoSearch}, somehow term misterey transorted to the pararent file
+    this.props.onSearchTermChange(term);
+  }
 
 }
 
--- src/index.js
+++ src/index.js
@@ -1,39 +1,65 @@
 // javascript modules in ES6.
+// lodash is js library to control the respose time of the states, proboly can also
+// control the time delay for other functions if applied
+import _ from 'lodash';
+// import the component from react framework
 import React, {Component} from 'react';
 // import ReactDoM
 import ReactDOM from 'react-dom';
+// import google API search
 import YTSearch from 'youtube-api-search';
 
 // import SearchBar from search_bar.js inside of components folder
 import SearchBar from './components/search_bar';
+// import videolist
 import VideoList from './components/video_list';
+// import VideoDetail
 import VideoDetail from './components/video_detail';
-// import key
+// import key, for securty reason, the api_key should be hide to the envirment varibles
 const API_KEY = "AIzaSyCk2w7xaSOKqprqBP67CEwW9YoCgE1pQ1k";
 
 
 
 // Create a new component. This component should produce some HTML.
 class App extends Component {
+  // props can get data from pararent js
   constructor(props) {
     super (props);
+    // initialize state, here we have an empty list videos, and selectedVideo
+    // also put a method videoSearch into the constructor
     this.state = {
       videos: [],
       selectedVideo: null
+
     };
-    YTSearch ({key:API_KEY, term:'qing yun zhi'}, (returnedVideos) =>{
-      this.setState({
-        videos:returnedVideos,
-        selectedVideo: returnedVideos[0]
-      });
-    });
+    this.videoSearch('react.js');
   }
+    // videoSearch method is also in the app component
+    videoSearch (term) {
+      // YTSearch is imported previously imported
+      YTSearch ({key:API_KEY, term:term}, (SearchedVideos) =>{ // this is a call back function for YTSearch
+        // here the state is set be the searched items, selectedVideo is set to be the first element in the array
+        this.setState({
+          videos:SearchedVideos,
+          selectedVideo: SearchedVideos[0]
+        });
+      });
+
+  }
+  // render create a jsx file and return them
   render (){
+    // here videoSearch is function
+    const videoSearch = _.debounce( (term) => {this.videoSearch(term)}, 300);
+    // inside of return: function on onSearchTermChange seems not defined in this document, need check *****
+    // video= {this.state.selectedVideo} pass this.state.selectedVideo to VideoList ReactDoM
+    // onVideoSelect = {selectedVideo => this.setState({selectedVideo}) selectedVideo and set state
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video= {this.state.selectedVideo}/>
-        <VideoList videos={this.state.videos}/>
+        <VideoList
+          onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos}/>
       </div>
     );
   };
